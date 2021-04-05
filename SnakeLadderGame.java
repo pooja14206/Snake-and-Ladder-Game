85efc3054c310fb4d31b0f278e2278b8fccc7b8c
Pooja <pooja.14206@gmail.com>
2021-04-05 13:17:23 +0530

    [POOJA] modify and add random function to find random between 1 to 6

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -5,6 +5,12 @@ public class SnakeLadderGame {
 		System.out.println("Wellcome to snake ladder game: ");
 
 		int startPosition = 0;
+		int minimum = 1;
+		int maximum = 6;
+
+		int randomValue = (int) Math.floor(Math.random() * (maximum - minimum + 1) + minimum);  //Math.random()*(max-min+1)+min
+
+		System.out.println("Random number between 1 to 6 = " + randomValue);
 
 	}
 
